cbf4a74c34f98fa721a3ef53cc3f02b7184663a8
Abhishek Jha <ajha.tips@gmail.com>
2025-11-29 12:17:49 +0000

    changes

--- websocket/ws_server.py
+++ websocket/ws_server.py
@@ -6,6 +6,7 @@ from app.services.ws_voice_stream import handle_ws_service
 async def handle_ws(websocket):
     return await handle_ws_service(websocket)
 
+
 async def main():
     print("Twilio WebSocket Server running on ws://0.0.0.0:9500/twilio-stream")
     async with websockets.serve(handle_ws, "0.0.0.0", 9500):
